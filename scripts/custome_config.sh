c615f8565b8f0c0e9705064dd88950c5844339f5
zack0617 <113736752@qq.com>
2022-12-28 22:36:04 +0800

    Change the default device to 4s

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,6 +3,7 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+sed -i -e 's/r2s/r4s/g' configs/rockchip/01-nanopi
 sed -i -e 's/HOSTNAME="FriendlyWrt"/HOSTNAME="NanoPi-R4SE"/g' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
 sed -i -e '/uci commit network/i \\tuci set network.lan.ipaddr=192.168.10.253' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
 sed -i -e '/uci commit network/i \\tuci set network.lan.gateway=192.168.10.1' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
