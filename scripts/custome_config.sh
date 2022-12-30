3544335d524a9eee05ed45ea5e99da29ded50173
zack0617 <113736752@qq.com>
2022-12-30 08:23:56 +0800

    Enalbe flow_offloading

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -5,6 +5,8 @@ sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/r2s/r4s/g' configs/rockchip/01-nanopi
 sed -i -e 's/HOSTNAME="FriendlyWrt"/HOSTNAME="NanoPi-R4SE"/g' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
+sed -i -e '/uci commit firewall/i \\tuci set firewall.@defaults[0].flow_offloading=1' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
+sed -i -e '/uci commit firewall/i \\tuci set firewall.@defaults[0].flow_offloading_hw=1' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
 sed -i -e '/uci commit network/i \\tuci set network.lan.ipaddr=192.168.10.253' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
 sed -i -e '/uci commit network/i \\tuci set network.lan.gateway=192.168.10.1' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
 sed -i -e '/uci commit network/i \\tuci set network.lan.dns=192.168.10.1' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
